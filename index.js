4ca3fd52b24cf5b24c54886bc19f79c2a6273d94
Decal <vumublne@naver.com>
2025-01-31 17:24:51 +0900

    add testMultiply & testDiv

--- index.js
+++ index.js
@@ -21,3 +21,11 @@ function testAdd() {
 function testSubtract() {
   console.log(subtract(2, 1) === 1);
 }
+
+function testMultiply() {
+  console.log(multiply(2, 3) === 6);
+}
+
+function testDiv() {
+  console.log(div(6, 3) === 2);
+}
